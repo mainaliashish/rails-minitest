1e8bad95a186b8bb32f7bae945df3d05d0eb0419
Ashish Mainali <ashishmainali@bajratechnologies.com>
2022-01-20 18:15:29 +0545

    feature: added password confirmation and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,16 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :categories
 
-  before_save { self.email = email.downcase }
+  before_save do
+    self.email = email.downcase
+    self.username = username.downcase
+  end
 
   enum role: { user: 0, admin: 1 }
 
-  validates :first_name, :last_name, :email, :username, presence: true
+  validates :first_name, :last_name, :email, :username, presence: true, length: { minimum: 5 }
   validates :email, :username, uniqueness: true
 
   has_secure_password(:password, validations: true)
-  validates :password, confirmation: true
+  validates :password, confirmation: true, length: { in: 5..20 }
 end
